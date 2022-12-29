7f6a117d185e1635df0d78a03dafbad2161ba618
Developer X <computer.tech.yt@gmail.com>
2022-12-30 04:01:51 +0500

    Fixed darkmode

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 var button = document.getElementById('moon')
 
 function changeClass() {
-    console.log(button.innerHTML)
-    if (button.innerHTML == '<i class="fa fa-sun-o" aria-hidden="true"></i>') {
-        document.body.classList.toggle('lightmode');
-        document.body.classList.toggle('darkmode');
-        document.getElementById('moon').innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
-    } else if (button.innerHTML == '<i class="fa fa-moon-o" aria-hidden="true"></i>') {
-        document.body.classList.toggle('darkmode');
-        document.body.classList.toggle('lightmode');
-        document.getElementById('moon').innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
+    if (document.body.classList.contains('lightmode')) {
+        document.body.classList.toggle('lightmode')
+        document.body.classList.toggle('darkmode')
+        button.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>'
+    } else if (document.body.classList.contains('darkmode')) {
+        document.body.classList.toggle('darkmode')
+        document.body.classList.toggle('lightmode')
+        button.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>'
+    } 
+}
+window.onload = () => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.toggle('darkmode')
+        button.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>'
+    } else {
+        document.body.classList.toggle('lightmode')
+        button.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>'
     }
-    
 }
