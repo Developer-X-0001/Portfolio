ec5302c9fa5aa93b288b776b8c04e2c75e63a5bc
Muhammad Dawood <computer.tech.yt@gmail.com>
2023-10-15 01:40:21 +0500

    Added some icons and mixed a bit of things

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ function changeClass() {
     if (document.body.classList.contains('lightmode')) {
         document.body.classList.toggle('lightmode')
         document.body.classList.toggle('darkmode')
-        button.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>'
+        button.innerHTML = '<i class="fa-regular fa-moon"></i>'
     } else if (document.body.classList.contains('darkmode')) {
         document.body.classList.toggle('darkmode')
         document.body.classList.toggle('lightmode')
-        button.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>'
+        button.innerHTML = '<i class="fa-regular fa-sun"></i>'
     } 
 }
 window.onload = () => {
     document.getElementById('ratingbox').classList.toggle('showRatings')
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.classList.toggle('darkmode')
-        button.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>'
+        button.innerHTML = '<i class="fa-regular fa-moon"></i>'
     } else {
         document.body.classList.toggle('lightmode')
-        button.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>'
+        button.innerHTML = '<i class="fa-regular fa-sun"></i>'
     }
 }
 
